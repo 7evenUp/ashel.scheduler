2953cf839b99c9f7d56263e08514e139c496c8f3
Artyom <sheludeshev.artyom@mail.ru>
2022-07-10 22:49:06 +0300

    Стилизировал текст

--- screens/GoalsScreen/GoalsScreen.tsx
+++ screens/GoalsScreen/GoalsScreen.tsx
@@ -1,19 +1,18 @@
-import { Dimensions, StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { BackgroundClipPath } from '../../components'
 import { palette } from '../../theme/default'
 
 const GoalsScreen = () => {
-  const {width, height} = Dimensions.get('window')
-
   return (
     <>
       <BackgroundClipPath backgroundColor={palette.red} angle='right'/>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>Никаких планов</Text>
-        <Text style={styles.text}>Только цели</Text>
-        <Text style={styles.text}>Width: {width}</Text>
-        <Text style={styles.text}>Height: {height}</Text>
+        <View style={styles.header}>
+          <Text style={styles.text}>Никаких планов</Text>
+          <Text style={styles.text}>Только цели</Text>
+        </View>
+        
       </SafeAreaView>
     </>
   )
@@ -24,6 +23,9 @@ export default GoalsScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    alignItems: 'center'
   },
   text: {
     color: 'white',
--- screens/IdeasScreen/IdeasScreen.tsx
+++ screens/IdeasScreen/IdeasScreen.tsx
@@ -1,17 +1,17 @@
-import { Dimensions, StyleSheet, Text } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { BackgroundClipPath } from '../../components'
 import { palette } from '../../theme/default'
 
 const IdeasScreen = () => {
-  const {width, height} = Dimensions.get('window')
   return (
     <>
       <BackgroundClipPath backgroundColor={palette.yellow} angle='left'/>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.text}>Пришла идея? Запиши</Text>
-        <Text style={styles.text}>Width: {width}</Text>
-        <Text style={styles.text}>Height: {height}</Text>
+        <View style={styles.header}>
+          <Text style={styles.text}>Пришла идея?</Text>
+          <Text style={styles.text}>Запиши</Text>
+        </View>
       </SafeAreaView>
     </>
   )
@@ -23,8 +23,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  header: {
+    alignItems: 'center'
+  },
   text: {
-    color: 'white',
+    color: 'black',
     fontSize: 24
   }
 })
